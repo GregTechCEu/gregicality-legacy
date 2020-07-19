cb1f34406458085affae2c6a7fda790b61031f0c
hjae78 <alexander.thoren1@gmail.com>
2020-07-19 10:45:47 +0200

    Fix duplicate recipes for cables with Polyphenylene Sulfide foils

--- src/main/java/gregicadditions/recipes/RecipeHandler.java
+++ src/main/java/gregicadditions/recipes/RecipeHandler.java
@@ -297,9 +297,6 @@ public class RecipeHandler {
                 case 8:
                 case 9:
                     RecipeMaps.ASSEMBLER_RECIPES.recipeBuilder().circuitMeta(24 + cableSize).input(OrePrefix.wireGtSingle, material, cableAmount).input(foil, PolyphenyleneSulfide, cableAmount).outputs(cableStack).duration(150).EUt(8).buildAndRegister();
-                default:
-                    RecipeMaps.ASSEMBLER_RECIPES.recipeBuilder().circuitMeta(24 + cableSize).input(OrePrefix.wireGtSingle, material, cableAmount).input(foil, PolyphenyleneSulfide, cableAmount).outputs(cableStack).duration(150).EUt(8).buildAndRegister();
-
             }
         }
         switch (tier) {
@@ -318,9 +315,6 @@ public class RecipeHandler {
             case 8:
             case 9:
                 RecipeMaps.ASSEMBLER_RECIPES.recipeBuilder().circuitMeta(24).input(wireGt, material).input(foil, PolyphenyleneSulfide, cableAmount).outputs(cableStack).duration(150).EUt(8).buildAndRegister();
-            default:
-                RecipeMaps.ASSEMBLER_RECIPES.recipeBuilder().circuitMeta(24).input(wireGt, material).input(foil, PolyphenyleneSulfide, cableAmount).outputs(cableStack).duration(150).EUt(8).buildAndRegister();
-
         }
 
     }
