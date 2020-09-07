1930a026446d91615dbc32ff02b7b7550c58f66b
Huneau romain <huneau.romain@gmail.com>
2020-09-07 22:57:25 +0200

    add config event

--- src/main/java/gregicadditions/CommonProxy.java
+++ src/main/java/gregicadditions/CommonProxy.java
@@ -3,11 +3,16 @@ package gregicadditions;
 import gregicadditions.fluid.GAMetaFluids;
 import gregicadditions.item.GAMetaItems;
 import gregicadditions.worldgen.WorldGenRegister;
+import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.ConfigManager;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import java.io.IOException;
 
+@Mod.EventBusSubscriber(modid = Gregicality.MODID)
 public class CommonProxy {
-
 
 
     public void preLoad() {
@@ -19,5 +24,12 @@ public class CommonProxy {
 
     public void onLoad() throws IOException {
         WorldGenRegister.init();
+    }
+
+    @SubscribeEvent
+    public static void syncConfigValues(ConfigChangedEvent.OnConfigChangedEvent event) {
+        if (event.getModID().equals(Gregicality.MODID)) {
+            ConfigManager.sync(Gregicality.MODID, Config.Type.INSTANCE);
+        }
     }
 }
