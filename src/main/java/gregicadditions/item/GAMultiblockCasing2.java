af9b1a4bf0a882d64fe8fdbb4f8b2e61ccc7d014
hjae78 <alexander.thoren1@gmail.com>
2020-09-27 18:10:48 +0200

    start on new naq reactors

---
+++ src/main/java/gregicadditions/item/GAMultiblockCasing2.java
@@ -0,0 +1,47 @@
+package gregicadditions.item;
+
+import gregtech.common.blocks.VariantBlock;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.util.IStringSerializable;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+
+public class GAMultiblockCasing2 extends VariantBlock<GAMultiblockCasing2.CasingType> {
+
+    public GAMultiblockCasing2() {
+        super(Material.IRON);
+        setTranslationKey("ga_multiblock_casing");
+        setHardness(5.0f);
+        setResistance(10.0f);
+        setSoundType(SoundType.METAL);
+        setHarvestLevel("wrench", 2);
+        setDefaultState(getState(CasingType.HYPER_CORE));
+    }
+
+    @Override
+    public boolean canCreatureSpawn(IBlockState state, IBlockAccess world, BlockPos pos, EntityLiving.SpawnPlacementType type) {
+        return false;
+    }
+
+    public enum CasingType implements IStringSerializable {
+
+        HYPER_CORE("hyper_core"),
+        STABILIZATION_CORE("stabilization_core"),
+        CONTAINMENT_CORE("containment_core");
+
+        private final String name;
+
+        CasingType(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String getName() {
+            return this.name;
+        }
+
+    }
+}
