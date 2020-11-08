ee7004b572f3f3b66d3b1e956699fcda9a0259d6
huneau romain <huneau.romain@gmail.com>
2020-11-08 12:16:24 +0100

    fix merge

--- src/main/java/gregicadditions/item/GAMetaItem.java
+++ src/main/java/gregicadditions/item/GAMetaItem.java
@@ -293,12 +293,12 @@ public class GAMetaItem extends MaterialMetaItem {
         DEGENERATE_RHENIUM_PLATE = addItem(432, "degenerate.rhenium.dust");
         PLATE_FIELD_SHAPE = addItem(431, "plate.field.shape");
 
-        ZBLAN = addItem(432, "zblan");
-        ZBLAN_INGOT = addItem(433, "zblan_ingot");
-        HOT_ANNEALED_ZBLAN_INGOT = addItem(434, "hot_annealed_zblan_ingot");
-        ZBLAN_FIBER = addItem(435, "zblan_fiber");
-        ERBIUM_DOPED_ZBLAN = addItem(436, "erbium_doped_zblan");
-        CLADDED_OPTICAL_FIBER_CORE = addItem(437, "cladded_optical_fiber_core");
+        ZBLAN = addItem(433, "zblan");
+        ZBLAN_INGOT = addItem(434, "zblan_ingot");
+        HOT_ANNEALED_ZBLAN_INGOT = addItem(435, "hot_annealed_zblan_ingot");
+        ZBLAN_FIBER = addItem(436, "zblan_fiber");
+        ERBIUM_DOPED_ZBLAN = addItem(437, "erbium_doped_zblan");
+        CLADDED_OPTICAL_FIBER_CORE = addItem(438, "cladded_optical_fiber_core");
 
         ThoriumRadioactive.waste = THORIUM_WASTE;
         Protactinium.waste = PROTACTINIUM_WASTE;
