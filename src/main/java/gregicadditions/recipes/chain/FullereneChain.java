2d0ea036725e1c7cbf611b001fee5027b944e833
hjae78 <alexander.thoren1@gmail.com>
2021-01-25 20:30:43 +0100

    reduce eu/t of potassium cyanide recipe

--- src/main/java/gregicadditions/recipes/chain/FullereneChain.java
+++ src/main/java/gregicadditions/recipes/chain/FullereneChain.java
@@ -138,8 +138,8 @@ public class FullereneChain {
                 .notConsumable(new IntCircuitIngredient(0))
                 .outputs(PotassiumCyanide.getItemStack())
                 .fluidOutputs(Water.getFluid(1000))
-                .EUt(30720)
-                .duration(500)
+                .EUt(7680)
+                .duration(1000)
                 .buildAndRegister();
         LARGE_CHEMICAL_RECIPES.recipeBuilder()
                 .input(dust, RockSalt, 2)
