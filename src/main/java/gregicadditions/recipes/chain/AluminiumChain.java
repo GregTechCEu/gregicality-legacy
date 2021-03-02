98ecf3b247ca47f03950686e98dc02c096ce030c
DStrand1 <dane@strandboge.com>
2021-03-01 19:17:22 -0600

    Finish AluminiumChain

--- src/main/java/gregicadditions/recipes/chain/AluminiumChain.java
+++ src/main/java/gregicadditions/recipes/chain/AluminiumChain.java
@@ -8,42 +8,47 @@ import static gregicadditions.recipes.GARecipeMaps.CHEMICAL_DEHYDRATOR_RECIPES;
 import static gregtech.api.recipes.RecipeMaps.*;
 import static gregtech.api.unification.material.Materials.*;
 import static gregtech.api.unification.ore.OrePrefix.dust;
-import static gregtech.api.unification.ore.OrePrefix.ingot;
 
 public class AluminiumChain {
     public static void init() {
+
+        // 2Al(OH)3 -> Al2O3 + 3H2O
         BLAST_RECIPES.recipeBuilder().duration(240).EUt(125).blastFurnaceTemp(1000)
-                .inputs(AluminiumHydroxide.getItemStack(2))
-                .outputs(Alumina.getItemStack())
+                .inputs(AluminiumHydroxide.getItemStack(14))
+                .outputs(Alumina.getItemStack(5))
                 .fluidOutputs(Water.getFluid(3000))
                 .buildAndRegister();
 
+        // 6NaOH + Al2O3 + 12HF -> 2Na3AlF6 + 9H2O
         CHEMICAL_RECIPES.recipeBuilder().duration(230).EUt(125)
-                .input(dust, SodiumHydroxide, 6)
-                .inputs(Alumina.getItemStack())
+                .input(dust, SodiumHydroxide, 18)
+                .inputs(Alumina.getItemStack(5))
                 .fluidInputs(HydrofluoricAcid.getFluid(12000))
                 .fluidOutputs(SodiumHexafluoroaluminate.getFluid(2000))
                 .fluidOutputs(Water.getFluid(9000))
                 .buildAndRegister();
 
+        // 2Al2O3 + Na3AlF6 -> 4Al + 3NaF + AlF3 + 6O
         ELECTROLYZER_RECIPES.recipeBuilder().duration(600).EUt(125)
-                .inputs(Alumina.getItemStack(2))
+                .inputs(Alumina.getItemStack(10))
                 .fluidInputs(SodiumHexafluoroaluminate.getFluid(1000))
                 .fluidOutputs(Aluminium.getFluid(576))
-                .outputs(OreDictUnifier.get(dust, SodiumFluoride, 3))
-                .outputs(AluminiumTrifluoride.getItemStack())
+                .fluidOutputs(Oxygen.getFluid(6000))
+                .outputs(OreDictUnifier.get(dust, SodiumFluoride, 6))
+                .outputs(AluminiumTrifluoride.getItemStack(4))
                 .buildAndRegister();
 
+        // 3NaF + AlF3 -> Na3AlF6
         CHEMICAL_RECIPES.recipeBuilder().duration(230).EUt(125)
-                .input(dust, SodiumFluoride, 3)
-                .inputs(AluminiumTrifluoride.getItemStack())
+                .input(dust, SodiumFluoride, 6)
+                .inputs(AluminiumTrifluoride.getItemStack(4))
                 .fluidOutputs(SodiumHexafluoroaluminate.getFluid(1000))
                 .buildAndRegister();
 
         MIXER_RECIPES.recipeBuilder().duration(240).EUt(32)
                 .fluidInputs(Water.getFluid(1000))
-                .input(dust, SodiumHydroxide)
-                .input(dust, Bauxite, 2)
+                .input(dust, SodiumHydroxide, 1)
+                .input(dust, Bauxite, 16)
                 .fluidOutputs(SodiumHydroxideBauxite.getFluid(4000))
                 .buildAndRegister();
 
@@ -62,13 +67,13 @@ public class AluminiumChain {
         CHEMICAL_DEHYDRATOR_RECIPES.recipeBuilder().duration(240).EUt(125)
                 .fluidInputs(PureAluminiumHydroxideSolution.getFluid(1000))
                 .notConsumable(new IntCircuitIngredient(0))
-                .outputs(AluminiumHydroxide.getItemStack())
+                .outputs(AluminiumHydroxide.getItemStack(7))
                 .buildAndRegister();
 
         CHEMICAL_DEHYDRATOR_RECIPES.recipeBuilder().duration(240).EUt(125)
-                .fluidInputs(PureAluminiumHydroxideSolution.getFluid(9000))
+                .fluidInputs(PureAluminiumHydroxideSolution.getFluid(8000))
                 .inputs(AluminiumHydroxide.getItemStack())
-                .outputs(AluminiumHydroxide.getItemStack(10))
+                .outputs(AluminiumHydroxide.getItemStack(64))
                 .buildAndRegister();
 
         CHEMICAL_RECIPES.recipeBuilder().duration(220).EUt(1000)
@@ -93,7 +98,7 @@ public class AluminiumChain {
                 .fluidInputs(RedSlurry.getFluid(1000))
                 .fluidInputs(SulfuricAcid.getFluid(1000))
                 .fluidOutputs(TitaniumSulfate.getFluid(1000))
-                .outputs(OreDictUnifier.get(dust, SiliconDioxide))
+                .outputs(OreDictUnifier.get(dust, SiliconDioxide, 3))
                 .buildAndRegister();
 
         CHEMICAL_RECIPES.recipeBuilder().duration(170).EUt(950)
