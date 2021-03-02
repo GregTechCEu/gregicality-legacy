496a6e4c2c958c1b42ba01fb9eacaed28f59de50
DStrand1 <dane@strandboge.com>
2021-03-02 01:49:29 -0600

    Finalize REEChain

--- src/main/java/gregicadditions/recipes/chain/REEChain.java
+++ src/main/java/gregicadditions/recipes/chain/REEChain.java
@@ -11,6 +11,30 @@ import static gregtech.api.unification.ore.OrePrefix.dustTiny;
 
 public class REEChain {
     public static void init() {
+
+        /*
+         * When I use [REE], it could be any of the below, in any combination,
+         * [La, Pr, Nd, Ce, Sc, Eu, Gd, Sm, Y, Tb, Dy, Ho, Er, Tm, Yt, Lu],
+         * being all Rare Earth Elements, excluding Promethium (Pm).
+         *
+         * Unknown compounds key:
+         * Acidic Salt Water: 3 == H2SO4(NaCl)3(H2O)3
+         * SulfuricBromineSolution: H2SO4Br(H2O)
+         * DebrominatedWater: H2O, basically
+         * HotVapourMixture: H2SO4Br(H2O)2
+         * DampBromine: Br(H2O)
+         * RareEarth: [REE]
+         * RareEarthHydroxidesSolution: Na[REE]-OH(H2O)
+         * RareEarthChloridesSolution: HNaOH[REE]-Cl(H2O)
+         * ThUSludge: ThU
+         * LaNdOxidesSolution: LaPrNdCeOx
+         * SmGdOxidesSolution: ScEuGdSmOx
+         * TbHoOxidesSolution: YTbDyHoOx
+         * ErLuOxidesSolution: ErTmYtLuOx
+         */
+
+        // 3NaCl(H2O) + 2Cl + H2SO4 -> H2SO4(NaCl)3(H2O)3
+        // Formula above multiplied up for simplicity
         CHEMICAL_RECIPES.recipeBuilder()
                 .fluidInputs(SaltWater.getFluid(1500))
                 .fluidInputs(Chlorine.getFluid(1000))
@@ -20,16 +44,17 @@ public class REEChain {
                 .duration(200)
                 .buildAndRegister();
 
+        // H2SO4(NaCl)3(H2O)3 -> 3NaCl + H2SO4Br(H2O) + 2H2O
         CENTRIFUGE_RECIPES.recipeBuilder()
                 .fluidInputs(AcidicSaltWater.getFluid(6000))
-                .outputs(OreDictUnifier.get(dust, Salt, 4))
-                .outputs(OreDictUnifier.get(dust, SodiumSulfate))
+                .outputs(OreDictUnifier.get(dust, Salt, 6))
                 .fluidOutputs(SulfuricBromineSolution.getFluid(3000))
-                .fluidOutputs(DebrominatedWater.getFluid(3000))
+                .fluidOutputs(DebrominatedWater.getFluid(2000))
                 .EUt(480)
                 .duration(200)
                 .buildAndRegister();
 
+        // H2SO4Br(H2O) + H2O -> H2SO4Br(H2O)2
         CHEMICAL_RECIPES.recipeBuilder()
                 .fluidInputs(SulfuricBromineSolution.getFluid(1500))
                 .fluidInputs(Steam.getFluid(1500))
@@ -38,14 +63,16 @@ public class REEChain {
                 .duration(150)
                 .buildAndRegister();
 
+        // ? -> H2SO4(H2O) + Br(H2O)
         CENTRIFUGE_RECIPES.recipeBuilder()
                 .fluidInputs(HotVapourMixture.getFluid(3000))
-                .fluidOutputs(DilutedHydrochloricAcid.getFluid(2000))
+                .fluidOutputs(DilutedSulfuricAcid.getFluid(2000))
                 .fluidOutputs(DampBromine.getFluid(1000))
                 .EUt(480)
                 .duration(200)
                 .buildAndRegister();
 
+        // Br(H2O) -> Br
         CHEMICAL_DEHYDRATOR_RECIPES.recipeBuilder()
                 .fluidInputs(DampBromine.getFluid(2000))
                 .fluidOutputs(Bromine.getFluid(1000))
@@ -53,6 +80,7 @@ public class REEChain {
                 .duration(500)
                 .buildAndRegister();
 
+        // CO + C3H6 + 2H -> C4H8O
         CHEMICAL_RECIPES.recipeBuilder()
                 .fluidInputs(CarbonMonoxde.getFluid(1000))
                 .fluidInputs(Propene.getFluid(1000))
@@ -61,6 +89,8 @@ public class REEChain {
                 .EUt(480)
                 .duration(200)
                 .buildAndRegister();
+
+        // 2C4H8O + 4H -> C8H18O + H2O
         CHEMICAL_RECIPES.recipeBuilder()
                 .fluidInputs(Butyraldehyde.getFluid(2000))
                 .fluidInputs(Hydrogen.getFluid(4000))
@@ -69,23 +99,29 @@ public class REEChain {
                 .EUt(480)
                 .duration(200)
                 .buildAndRegister();
+
+        // 5C8H18O + 0.5P4O10 -> 2C16H35O4P + 2C4H8 + 2H2O
         LARGE_CHEMICAL_RECIPES.recipeBuilder()
-                .fluidInputs(Ethylhexanol.getFluid(6000))
-                .input(dust, PhosphorousPentoxide)
+                .fluidInputs(Ethylhexanol.getFluid(5000))
+                .input(dust, PhosphorousPentoxide, 7)
                 .fluidOutputs(DiethylhexylPhosphoricAcid.getFluid(2000))
-                .fluidOutputs(Butene.getFluid(4000))
-                .fluidOutputs(Water.getFluid(1000))
+                .fluidOutputs(Butene.getFluid(2000))
+                .fluidOutputs(Water.getFluid(2000))
                 .EUt(480)
                 .duration(200)
                 .buildAndRegister();
+
+        // [REE] + NaOH + H2O -> Na[REE]-OH(H2O)
         MIXER_RECIPES.recipeBuilder()
                 .input(dust, RareEarth)
-                .input(dust, SodiumHydroxide)
+                .input(dust, SodiumHydroxide, 3)
                 .fluidInputs(Water.getFluid(1000))
                 .fluidOutputs(RareEarthHydroxidesSolution.getFluid(1000))
                 .EUt(480)
                 .duration(200)
                 .buildAndRegister();
+
+        // HCl + Na[REE]-OH(H2O) -> ThU + HNaOH[REE]-Cl(H2O)
         CHEMICAL_RECIPES.recipeBuilder()
                 .fluidInputs(HydrochloricAcid.getFluid(1000))
                 .fluidInputs(RareEarthHydroxidesSolution.getFluid(1000))
@@ -94,10 +130,12 @@ public class REEChain {
                 .EUt(480)
                 .duration(200)
                 .buildAndRegister();
+
+        // HNaOH[REE]-Cl(H2O) + C16H35O4P(cat.) -> NaOH + [REE](sep.) + HCl(H2O)
         LARGE_CENTRIFUGE_RECIPES.recipeBuilder()
                 .fluidInputs(RareEarthChloridesSolution.getFluid(1000))
-                .fluidInputs(DiethylhexylPhosphoricAcid.getFluid(1000))
-                .outputs(OreDictUnifier.get(dust, SodiumHydroxide))
+                .fluidInputs(DiethylhexylPhosphoricAcid.getFluid(10))
+                .outputs(OreDictUnifier.get(dust, SodiumHydroxide, 3))
                 .fluidOutputs(LaNdOxidesSolution.getFluid(250))
                 .fluidOutputs(SmGdOxidesSolution.getFluid(250))
                 .fluidOutputs(TbHoOxidesSolution.getFluid(250))
@@ -106,42 +144,52 @@ public class REEChain {
                 .EUt(480)
                 .duration(600)
                 .buildAndRegister();
+
+        // 4LaPrNdCeOx -> La2O3 + Pr2O3 + Nd2O3 + Ce2O3 (each 43% +2.75%)
         CENTRIFUGE_RECIPES.recipeBuilder()
                 .fluidInputs(LaNdOxidesSolution.getFluid(4000))
-                .chancedOutput(LanthanumOxide.getItemStack(), 4300, 275)
-                .chancedOutput(PraseodymiumOxide.getItemStack(), 4300, 275)
-                .chancedOutput(NeodymiumOxide.getItemStack(), 4300, 275)
-                .chancedOutput(CeriumOxide.getItemStack(), 4300, 275)
+                .chancedOutput(LanthanumOxide.getItemStack(5), 4300, 275)
+                .chancedOutput(PraseodymiumOxide.getItemStack(5), 4300, 275)
+                .chancedOutput(NeodymiumOxide.getItemStack(5), 4300, 275)
+                .chancedOutput(CeriumOxide.getItemStack(5), 4300, 275)
                 .EUt(480)
                 .duration(250)
                 .buildAndRegister();
+
+        // 4ScEuGdSmOx -> Sc2O3 + Eu2O3 + Gd2O3 + Sm2O3 (each 43% +2.75%)
         CENTRIFUGE_RECIPES.recipeBuilder()
                 .fluidInputs(SmGdOxidesSolution.getFluid(4000))
-                .chancedOutput(ScandiumOxide.getItemStack(), 4300, 275)
-                .chancedOutput(EuropiumOxide.getItemStack(), 4300, 275)
-                .chancedOutput(GadoliniumOxide.getItemStack(), 4300, 275)
-                .chancedOutput(SamariumOxide.getItemStack(), 4300, 275)
+                .chancedOutput(ScandiumOxide.getItemStack(5), 4300, 275)
+                .chancedOutput(EuropiumOxide.getItemStack(5), 4300, 275)
+                .chancedOutput(GadoliniumOxide.getItemStack(5), 4300, 275)
+                .chancedOutput(SamariumOxide.getItemStack(5), 4300, 275)
                 .EUt(480)
                 .duration(250)
                 .buildAndRegister();
+
+        // 4YTbDyHoOx -> Y2O3 + Tb2O3 + Dy2O3 + Ho2O3 (each 43% +2.75%)
         CENTRIFUGE_RECIPES.recipeBuilder()
                 .fluidInputs(TbHoOxidesSolution.getFluid(4000))
-                .chancedOutput(OreDictUnifier.get(dust, YttriumOxide), 4300, 275)
-                .chancedOutput(TerbiumOxide.getItemStack(), 4300, 275)
-                .chancedOutput(DysprosiumOxide.getItemStack(), 4300, 275)
-                .chancedOutput(HolmiumOxide.getItemStack(), 4300, 275)
+                .chancedOutput(OreDictUnifier.get(dust, YttriumOxide, 5), 4300, 275)
+                .chancedOutput(TerbiumOxide.getItemStack(5), 4300, 275)
+                .chancedOutput(DysprosiumOxide.getItemStack(5), 4300, 275)
+                .chancedOutput(HolmiumOxide.getItemStack(5), 4300, 275)
                 .EUt(480)
                 .duration(250)
                 .buildAndRegister();
+
+        // 4ErTmYtLuOx -> Er2O3 + Tm2O3 + Yt2O3 + Lu2O3 (each 43% +2.75%)
         CENTRIFUGE_RECIPES.recipeBuilder()
                 .fluidInputs(ErLuOxidesSolution.getFluid(4000))
-                .chancedOutput(ErbiumOxide.getItemStack(), 4300, 275)
-                .chancedOutput(ThuliumOxide.getItemStack(), 4300, 275)
-                .chancedOutput(YtterbiumOxide.getItemStack(), 4300, 275)
-                .chancedOutput(LutetiumOxide.getItemStack(), 4300, 275)
+                .chancedOutput(ErbiumOxide.getItemStack(5), 4300, 275)
+                .chancedOutput(ThuliumOxide.getItemStack(5), 4300, 275)
+                .chancedOutput(YtterbiumOxide.getItemStack(5), 4300, 275)
+                .chancedOutput(LutetiumOxide.getItemStack(5), 4300, 275)
                 .EUt(480)
                 .duration(250)
                 .buildAndRegister();
+
+        // 3C + 2La2O3 -> 4La + 3CO2
         BLAST_RECIPES.recipeBuilder()
                 .blastFurnaceTemp(2500)
                 .input(dust, Carbon, 3)
@@ -151,6 +199,8 @@ public class REEChain {
                 .EUt(480)
                 .duration(100)
                 .buildAndRegister();
+
+        // 3C + 2Pr2O3 -> 4Pr + 3CO2
         BLAST_RECIPES.recipeBuilder()
                 .blastFurnaceTemp(2500)
                 .input(dust, Carbon, 3)
@@ -160,6 +210,8 @@ public class REEChain {
                 .EUt(480)
                 .duration(100)
                 .buildAndRegister();
+
+        // 3C + 2Nd2O3 -> 4Nd + 3CO2
         BLAST_RECIPES.recipeBuilder()
                 .blastFurnaceTemp(2500)
                 .input(dust, Carbon, 3)
@@ -169,6 +221,8 @@ public class REEChain {
                 .EUt(480)
                 .duration(100)
                 .buildAndRegister();
+
+        // 3C + 2Ce2O3 -> 4Ce + 3CO2
         BLAST_RECIPES.recipeBuilder()
                 .blastFurnaceTemp(2500)
                 .input(dust, Carbon, 3)
@@ -179,6 +233,7 @@ public class REEChain {
                 .duration(100)
                 .buildAndRegister();
 
+        // 3C + 2Sc2O3 -> 4Sc + 3CO2
         BLAST_RECIPES.recipeBuilder()
                 .blastFurnaceTemp(2500)
                 .input(dust, Carbon, 3)
@@ -188,6 +243,8 @@ public class REEChain {
                 .EUt(480)
                 .duration(100)
                 .buildAndRegister();
+
+        // 3C + 2Eu2O3 -> 4Eu + 3CO2
         BLAST_RECIPES.recipeBuilder()
                 .blastFurnaceTemp(2500)
                 .input(dust, Carbon, 3)
@@ -197,6 +254,8 @@ public class REEChain {
                 .EUt(480)
                 .duration(100)
                 .buildAndRegister();
+
+        // 3C + 2Gd2O3 -> 4Gd + 3CO2
         BLAST_RECIPES.recipeBuilder()
                 .blastFurnaceTemp(2500)
                 .input(dust, Carbon, 3)
@@ -206,6 +265,8 @@ public class REEChain {
                 .EUt(480)
                 .duration(100)
                 .buildAndRegister();
+
+        // 3C + 2Sm2O3 -> 4Sm + 3CO2
         BLAST_RECIPES.recipeBuilder()
                 .blastFurnaceTemp(2500)
                 .input(dust, Carbon, 3)
@@ -216,6 +277,7 @@ public class REEChain {
                 .duration(100)
                 .buildAndRegister();
 
+        // 3C + 2Y2O3 -> 4Y + 3CO2
         BLAST_RECIPES.recipeBuilder()
                 .blastFurnaceTemp(2500)
                 .input(dust, Carbon,3)
@@ -225,71 +287,85 @@ public class REEChain {
                 .EUt(480)
                 .duration(100)
                 .buildAndRegister();
+
+        // 3C + 2Tb2O3 -> 4Tb + 3CO2
         BLAST_RECIPES.recipeBuilder()
                 .blastFurnaceTemp(2500)
                 .input(dust, Carbon,3)
-                .inputs(TerbiumOxide.getItemStack(2))
+                .inputs(TerbiumOxide.getItemStack(10))
                 .outputs(OreDictUnifier.get(dust, Terbium,4))
                 .fluidOutputs(CarbonDioxide.getFluid(3000))
                 .EUt(480)
                 .duration(100)
                 .buildAndRegister();
+
+        // 3C + 2Dy2O3 -> 4Dy + 3CO2
         BLAST_RECIPES.recipeBuilder()
                 .blastFurnaceTemp(2500)
                 .input(dust, Carbon,3)
-                .inputs(DysprosiumOxide.getItemStack(2))
+                .inputs(DysprosiumOxide.getItemStack(10))
                 .outputs(OreDictUnifier.get(dust, Dysprosium,4))
                 .fluidOutputs(CarbonDioxide.getFluid(3000))
                 .EUt(480)
                 .duration(100)
                 .buildAndRegister();
+
+        // 3C + 2Ho2O3 -> 4Ho + 3CO2
         BLAST_RECIPES.recipeBuilder()
                 .blastFurnaceTemp(2500)
                 .input(dust, Carbon,3)
-                .inputs(HolmiumOxide.getItemStack(2))
+                .inputs(HolmiumOxide.getItemStack(10))
                 .outputs(OreDictUnifier.get(dust, Holmium,4))
                 .fluidOutputs(CarbonDioxide.getFluid(3000))
                 .EUt(480)
                 .duration(100)
                 .buildAndRegister();
 
+        // 3C + 2Er2O3 -> 4Er + 3CO2
         BLAST_RECIPES.recipeBuilder()
                 .blastFurnaceTemp(2500)
                 .input(dust, Carbon,3)
-                .inputs(ErbiumOxide.getItemStack(2))
+                .inputs(ErbiumOxide.getItemStack(10))
                 .outputs(OreDictUnifier.get(dust, Erbium,4))
                 .fluidOutputs(CarbonDioxide.getFluid(3000))
                 .EUt(480)
                 .duration(100)
                 .buildAndRegister();
+
+        // 3C + 2Tm2O3 -> 4Tm + 3CO2
         BLAST_RECIPES.recipeBuilder()
                 .blastFurnaceTemp(2500)
                 .input(dust, Carbon,3)
-                .inputs(ThuliumOxide.getItemStack(2))
+                .inputs(ThuliumOxide.getItemStack(10))
                 .outputs(OreDictUnifier.get(dust, Thulium,4))
                 .fluidOutputs(CarbonDioxide.getFluid(3000))
                 .EUt(480)
                 .duration(100)
                 .buildAndRegister();
+
+        // 3C + Yt2O3 -> 4Yt + 3CO2
         BLAST_RECIPES.recipeBuilder()
                 .blastFurnaceTemp(2500)
                 .input(dust, Carbon,3)
-                .inputs(YtterbiumOxide.getItemStack(2))
+                .inputs(YtterbiumOxide.getItemStack(10))
                 .outputs(OreDictUnifier.get(dust, Ytterbium,4))
                 .fluidOutputs(CarbonDioxide.getFluid(3000))
                 .EUt(480)
                 .duration(100)
                 .buildAndRegister();
+
+        // 3C + 2Lu2O3 -> 4Lu + 3CO2
         BLAST_RECIPES.recipeBuilder()
                 .blastFurnaceTemp(2500)
                 .input(dust, Carbon,3)
-                .inputs(LutetiumOxide.getItemStack(2))
+                .inputs(LutetiumOxide.getItemStack(10))
                 .outputs(OreDictUnifier.get(dust, Lutetium,4))
                 .fluidOutputs(CarbonDioxide.getFluid(3000))
                 .EUt(480)
                 .duration(100)
                 .buildAndRegister();
 
+        // 2ThU -> Th(20%) + U(20%)
         CENTRIFUGE_RECIPES.recipeBuilder()
                 .inputs(ThUSludge.getItemStack(2))
                 .chancedOutput(OreDictUnifier.get(dustTiny, Thorium), 2000, 150)
