2cb71ffeaaec348180a31b5fcb12d731ae1b29a4
DStrand1 <dane@strandboge.com>
2021-03-01 19:36:36 -0600

    Finalize BariumChain

--- src/main/java/gregicadditions/recipes/chain/BariumChain.java
+++ src/main/java/gregicadditions/recipes/chain/BariumChain.java
@@ -10,48 +10,61 @@ import static gregtech.api.unification.ore.OrePrefix.*;
 
 public class BariumChain {
     public static void init() {
+
+        // Ba + 2HCl -> BaCl2 + 2H
         CHEMICAL_RECIPES.recipeBuilder().duration(100).EUt(480)
                 .input(dust, Barium)
                 .fluidInputs(HydrochloricAcid.getFluid(2000))
-                .outputs(BariumChloride.getItemStack())
+                .outputs(BariumChloride.getItemStack(3))
                 .fluidOutputs(Hydrogen.getFluid(2000))
                 .buildAndRegister();
+
+        // BaCl2 + H2SO4 -> BaSO4 + HCl
         CHEMICAL_RECIPES.recipeBuilder().duration(50).EUt(480)
-                .inputs(BariumChloride.getItemStack())
+                .inputs(BariumChloride.getItemStack(3))
                 .fluidInputs(SulfuricAcid.getFluid(1000))
-                .outputs(OreDictUnifier.get(dust, Barite))
+                .outputs(OreDictUnifier.get(dust, Barite, 6))
                 .fluidOutputs(HydrochloricAcid.getFluid(2000))
                 .buildAndRegister();
+
+        // BaSO4 + 2C -> BaS + 2CO2
         BLAST_RECIPES.recipeBuilder().duration(480).EUt(500).blastFurnaceTemp(1200)
-                .input(dust, Barite)
+                .input(dust, Barite, 6)
                 .input(dust, Carbon, 2)
                 .outputs(BariumSulfide.getItemStack())
                 .fluidOutputs(CarbonDioxide.getFluid(2000))
                 .buildAndRegister();
+
+        // BaS + H2O + CO2 -> BaCO3 + H2S
         CHEMICAL_RECIPES.recipeBuilder().duration(400).EUt(500)
-                .inputs(BariumSulfide.getItemStack())
+                .inputs(BariumSulfide.getItemStack(2))
                 .fluidInputs(Water.getFluid(1000))
                 .fluidInputs(CarbonDioxide.getFluid(1000))
-                .outputs(BariumCarbonate.getItemStack())
+                .outputs(BariumCarbonate.getItemStack(5))
                 .fluidOutputs(HydrogenSulfide.getFluid(1000))
                 .buildAndRegister();
+
+        // BaCO3 -> CO2 + BaO
         CHEMICAL_RECIPES.recipeBuilder().duration(100).EUt(250)
-                .inputs(BariumCarbonate.getItemStack())
-                .outputs(BariumOxide.getItemStack())
+                .inputs(BariumCarbonate.getItemStack(5))
+                .outputs(BariumOxide.getItemStack(2))
                 .notConsumable(new IntCircuitIngredient(0))
                 .fluidOutputs(CarbonDioxide.getFluid(1000))
                 .buildAndRegister();
+
+        // 4BaO + 2Al -> 3Ba + Al2BaO4
         BLAST_RECIPES.recipeBuilder().duration(240).EUt(500).blastFurnaceTemp(700)
-                .inputs(BariumOxide.getItemStack(2))
+                .inputs(BariumOxide.getItemStack(8))
                 .input(dustSmall, Aluminium, 8)
-                .outputs(OreDictUnifier.get(ingot, Barium))
-                .outputs(BariumAluminate.getItemStack(2))
+                .outputs(OreDictUnifier.get(ingot, Barium, 3))
+                .outputs(BariumAluminate.getItemStack(7))
                 .buildAndRegister();
+        
+        // 2Al2BaO4 -> BaO + Al2O3
         CENTRIFUGE_RECIPES.recipeBuilder().duration(300).EUt(500)
-                .inputs(BariumAluminate.getItemStack(2))
-                .outputs(BariumOxide.getItemStack())
-                .outputs(Alumina.getItemStack(2))
+                .inputs(BariumAluminate.getItemStack(14))
+                .outputs(BariumOxide.getItemStack(2))
+                .outputs(Alumina.getItemStack(5))
                 .buildAndRegister();
     }
-
 }
