c2a0a0851e325a74c1004ba283f027628e009e8e
TechLord22 <swarren2022@gmail.com>
2021-03-06 12:59:58 -0500

    Fix Iodine Chain

--- src/main/java/gregicadditions/recipes/chain/IodineChain.java
+++ src/main/java/gregicadditions/recipes/chain/IodineChain.java
@@ -11,27 +11,32 @@ import static gregtech.api.unification.ore.OrePrefix.dust;
 public class IodineChain {
     public static void init() {
 
+        // NaNO3·KNO3·KCl·NaIO3 + H2O = [NaNO3·KNO3·KCl·NaIO3 + H2O]
         MIXER_RECIPES.recipeBuilder().duration(240).EUt(1250)
                 .input(dust, Caliche)
                 .fluidInputs(Water.getFluid(1000))
                 .fluidOutputs(CalicheIodateBrine.getFluid(1000))
                 .buildAndRegister();
 
+        // [NaNO3·KNO3·KCl·NaIO3 + H2O] + 3H2O + 3SO2 = [NaNO3·KNO3·KCl·NaI + H2O] + 3H2SO4
         CHEMICAL_RECIPES.recipeBuilder().duration(160).EUt(1250)
                 .fluidInputs(CalicheIodateBrine.getFluid(1000))
-                .fluidInputs(SulfurDioxide.getFluid(1000))
-                .input(dust, Carbon)
+                .fluidInputs(SulfurDioxide.getFluid(3000))
+                .fluidInputs(Water.getFluid(3000))
                 .fluidOutputs(IodideSolution.getFluid(1000))
-                .fluidOutputs(CarbonMonoxde.getFluid(1000))
-                .outputs(OreDictUnifier.get(dust, SodiumSulfate, 7))
+                .fluidOutputs(SulfuricAcid.getFluid(3000))
                 .buildAndRegister();
 
-        CHEMICAL_RECIPES.recipeBuilder().duration(140).EUt(1250)
-                .fluidInputs(CalicheIodateBrine.getFluid(167))
-                .fluidInputs(IodideSolution.getFluid(833))
-                .fluidOutputs(CalicheIodineBrine.getFluid(1000))
+        // 5[NaNO3·KNO3·KCl·NaI] + [NaNO3·KNO3·KCl·NaIO3 + H2O] ->  6[NaNO3·KNO3·KCl·NaOH + H2O + I]
+        // 167/833 = ~0.2, ratio changed to cleaner 1:5
+        // This is correct
+        MIXER_RECIPES.recipeBuilder().duration(140).EUt(1250)
+                .fluidInputs(CalicheIodateBrine.getFluid(1000))
+                .fluidInputs(IodideSolution.getFluid(5000))
+                .fluidOutputs(CalicheIodineBrine.getFluid(6000))
                 .buildAndRegister();
 
+        // [NaNO3·KNO3·KCl·NaOH + H2O + I] +  Kerosene ->  [NaNO3·KNO3·KCl·NaOH + H2O] + [Kerosene + I]
         CHEMICAL_RECIPES.recipeBuilder().duration(90).EUt(600)
                 .fluidInputs(CalicheIodineBrine.getFluid(1000))
                 .fluidInputs(Kerosene.getFluid(1000))
@@ -39,36 +44,44 @@ public class IodineChain {
                 .fluidOutputs(KeroseneIodineSolution.getFluid(1000))
                 .buildAndRegister();
 
+        // [Kerosene + I] = Kerosene + I
         CENTRIFUGE_RECIPES.recipeBuilder().duration(90).EUt(600)
                 .fluidInputs(KeroseneIodineSolution.getFluid(1000))
                 .fluidOutputs(Kerosene.getFluid(1000))
                 .outputs(OreDictUnifier.get(dust, Iodine))
                 .buildAndRegister();
 
+        // [NaNO3·KNO3·KCl·NaOH + H2O] = NaNO3 + KNO3 + KCl + NaOH + H2O (Water voided - Dehydrator)
         CHEMICAL_DEHYDRATOR_RECIPES.recipeBuilder().duration(120).EUt(750)
                 .fluidInputs(CalicheNitrateSolution.getFluid(1000))
+                .fluidInputs(Nitrogen.getFluid(1000))
                 .outputs(OreDictUnifier.get(dust, Niter, 5))
                 .outputs(OreDictUnifier.get(dust, RockSalt, 2))
+                .outputs(OreDictUnifier.get(dust, SodiumHydroxide, 3))
                 .buildAndRegister();
 
+        // 2[I + Oil] = Oil + I?
         CENTRIFUGE_RECIPES.recipeBuilder().duration(160).EUt(30)
                 .fluidInputs(IodizedOil.getFluid(2000))
                 .fluidOutputs(Oil.getFluid(1000))
                 .fluidOutputs(IodizedBrine.getFluid(1000))
                 .buildAndRegister();
 
+        // I? + 0.3Cl = 1.3I??
         MIXER_RECIPES.recipeBuilder().duration(240).EUt(1250)
                 .fluidInputs(IodizedBrine.getFluid(1000))
                 .fluidInputs(Chlorine.getFluid(300))
                 .fluidOutputs(IodineBrineMix.getFluid(1300))
                 .buildAndRegister();
 
+        // 1.3I?? = Br? + 0.3I?
         CENTRIFUGE_RECIPES.recipeBuilder().duration(150).EUt(1250)
                 .fluidInputs(IodineBrineMix.getFluid(1300))
                 .fluidOutputs(BrominatedBrine.getFluid(1000))
                 .fluidOutputs(IodineSlurry.getFluid(300))
                 .buildAndRegister();
 
+        // I? = I
         CHEMICAL_DEHYDRATOR_RECIPES.recipeBuilder().duration(230).EUt(600)
                 .fluidInputs(IodineSlurry.getFluid(1000))
                 .outputs(OreDictUnifier.get(dust, Iodine))
