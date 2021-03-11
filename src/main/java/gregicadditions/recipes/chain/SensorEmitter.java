d7f96436005ce1ab1fda1b01cc7a6fb8a9ac1a8d
hjae78 <alexander.thoren1@gmail.com>
2021-03-11 11:45:38 +0100

    Fix imports

--- src/main/java/gregicadditions/recipes/chain/SensorEmitter.java
+++ src/main/java/gregicadditions/recipes/chain/SensorEmitter.java
@@ -3,12 +3,11 @@ package gregicadditions.recipes.chain;
 import gregtech.api.unification.OreDictUnifier;
 
 import static gregicadditions.GAMaterials.*;
-import static gregicadditions.item.GAMetaItems.UVA_HALIDE_LAMP;
+import static gregicadditions.item.GAMetaItems.*;
 import static gregicadditions.recipes.GARecipeMaps.*;
 import static gregtech.api.recipes.RecipeMaps.*;
 import static gregtech.api.unification.material.Materials.*;
-import static gregtech.api.unification.ore.OrePrefix.dust;
-import static gregtech.api.unification.ore.OrePrefix.wireFine;
+import static gregtech.api.unification.ore.OrePrefix.*;
 
 public class SensorEmitter {
     public static void init() {
