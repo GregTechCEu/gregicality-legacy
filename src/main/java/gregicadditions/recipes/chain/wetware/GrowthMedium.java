76418efafda8087a3c2c895270baeb52be90aa99
DStrand1 <dane@strandboge.com>
2021-03-20 21:12:47 -0500

    Finalize GrowthMedium

--- src/main/java/gregicadditions/recipes/chain/wetware/GrowthMedium.java
+++ src/main/java/gregicadditions/recipes/chain/wetware/GrowthMedium.java
@@ -15,7 +15,8 @@ import static gregtech.api.unification.ore.OrePrefix.dust;
 
 public class GrowthMedium {
     public static void init() {
-        // 2CaO + 5C = CO2 + 2CaC2
+
+        // 2CaO + 5C -> CO2 + 2CaC2
         BLAST_RECIPES.recipeBuilder()
                 .input(dust, Quicklime, 4)
                 .input(dust, Carbon, 5)
@@ -25,7 +26,8 @@ public class GrowthMedium {
                 .EUt(7680)
                 .duration(100)
                 .buildAndRegister();
-        // CaC2 + 2H2O = Ca(OH)2 + C2H2
+
+        // CaC2 + 2H2O -> Ca(OH)2 + C2H2
         BLAST_RECIPES.recipeBuilder()
                 .inputs(CalciumCarbide.getItemStack(3))
                 .fluidInputs(Steam.getFluid(2000))
@@ -35,6 +37,7 @@ public class GrowthMedium {
                 .EUt(30720)
                 .duration(10)
                 .buildAndRegister();
+
         // Ca(OH)2 + 2HCl -> 2H2O + CaCl2
         CHEMICAL_RECIPES.recipeBuilder()
                 .inputs(CalciumHydroxide.getItemStack(5))
@@ -44,6 +47,8 @@ public class GrowthMedium {
                 .EUt(480)
                 .duration(50)
                 .buildAndRegister();
+
+        // O + CH3OH -> CH2O + H2O
         CHEMICAL_RECIPES.recipeBuilder()
                 .notConsumable(dust, Silver)
                 .fluidInputs(Oxygen.getFluid(1000))
@@ -53,6 +58,8 @@ public class GrowthMedium {
                 .EUt(30720)
                 .duration(100)
                 .buildAndRegister();
+
+        // CH2O + C2H2 -> C3H4O
         CHEMICAL_RECIPES.recipeBuilder()
                 .notConsumable(dust, Copper)
                 .fluidInputs(Formaldehyde.getFluid(1000))
@@ -61,42 +68,52 @@ public class GrowthMedium {
                 .EUt(30720)
                 .duration(100)
                 .buildAndRegister();
+
+        // C3H4O + 2Cl -> C3H3Cl + HClO
         CHEMICAL_RECIPES.recipeBuilder()
                 .fluidInputs(PropargylAlcohol.getFluid(1000))
-                .fluidInputs(Chlorine.getFluid(1000))
+                .fluidInputs(Chlorine.getFluid(2000))
                 .fluidOutputs(PropargylChloride.getFluid(1000))
                 .fluidOutputs(HypochlorousAcid.getFluid(1000))
                 .EUt(30720)
                 .duration(100)
                 .buildAndRegister();
+
         FLUID_EXTRACTION_RECIPES.recipeBuilder()
                 .inputs(MetaItems.RUBBER_DROP.getStackForm())
                 .fluidOutputs(Resin.getFluid(100))
                 .EUt(32)
                 .duration(100)
                 .buildAndRegister();
+
         DISTILLATION_RECIPES.recipeBuilder()
                 .fluidInputs(Resin.getFluid(1000))
                 .fluidOutputs(Turpentine.getFluid(200))
                 .EUt(480)
                 .duration(500)
                 .buildAndRegister();
+
+        // C10H16 + H2SO4 -> C10H16 + H2SO4
         CHEMICAL_RECIPES.recipeBuilder()
                 .fluidInputs(Turpentine.getFluid(1000))
                 .fluidInputs(SulfuricAcid.getFluid(1000))
-                .outputs(BetaPinene.getItemStack())
+                .outputs(BetaPinene.getItemStack(26))
                 .fluidOutputs(SulfuricAcid.getFluid(1000))
                 .EUt(30720)
                 .duration(50)
                 .buildAndRegister();
+
+        // C10H16 + 2C5H8 + 2O -> 2C10H16O
         CHEMICAL_RECIPES.recipeBuilder()
-                .inputs(BetaPinene.getItemStack())
+                .inputs(BetaPinene.getItemStack(26))
                 .fluidInputs(Isoprene.getFluid(2000))
                 .fluidInputs(Oxygen.getFluid(2000))
                 .fluidOutputs(Citral.getFluid(2000))
                 .EUt(30720)
                 .duration(100)
                 .buildAndRegister();
+
+        // C10H16O + C3H6O -> C13H20O + H2O
         CHEMICAL_RECIPES.recipeBuilder()
                 .fluidInputs(Citral.getFluid(1000))
                 .fluidInputs(Acetone.getFluid(1000))
@@ -105,6 +122,8 @@ public class GrowthMedium {
                 .EUt(30720)
                 .duration(250)
                 .buildAndRegister();
+
+        // 25C13H20O + 5C3H3Cl -> 17C20H30O + 8O + 5HCl
         LARGE_CHEMICAL_RECIPES.recipeBuilder()
                 .fluidInputs(BetaIonone.getFluid(25000))
                 .fluidInputs(PropargylChloride.getFluid(5000))
@@ -114,12 +133,15 @@ public class GrowthMedium {
                 .EUt(30720)
                 .duration(200)
                 .buildAndRegister();
+
+        // TODO This needs to be better, Zalgo was working on something
         MACERATOR_RECIPES.recipeBuilder()
                 .inputs(new ItemStack(Items.APPLE))
                 .chancedOutput(Yeast.getItemStack(), 500, 250)
                 .EUt(120)
                 .duration(50)
                 .buildAndRegister();
+
         BIO_REACTOR_RECIPES.recipeBuilder()
                 .fluidInputs(Biomass.getFluid(1000))
                 .inputs(Yeast.getItemStack())
@@ -128,6 +150,8 @@ public class GrowthMedium {
                 .EUt(30720)
                 .duration(200)
                 .buildAndRegister();
+
+        // C2H4 + O -> C2H4O
         CHEMICAL_RECIPES.recipeBuilder()
                 .notConsumable(new IntCircuitIngredient(10))
                 .fluidInputs(Ethylene.getFluid(1000))
@@ -136,6 +160,8 @@ public class GrowthMedium {
                 .EUt(500)
                 .duration(400)
                 .buildAndRegister();
+
+        // NH3 + C2H4O -> C2H7NO
         CHEMICAL_RECIPES.recipeBuilder()
                 .fluidInputs(Ammonia.getFluid(1000))
                 .fluidInputs(EthyleneOxide.getFluid(1000))
@@ -143,6 +169,7 @@ public class GrowthMedium {
                 .EUt(30720)
                 .duration(150)
                 .buildAndRegister();
+
         BIO_REACTOR_RECIPES.recipeBuilder()
                 .inputs(CupriavidusNecator.getItemStack())
                 .input(dust, Sugar)
@@ -152,6 +179,7 @@ public class GrowthMedium {
                 .EUt(30720)
                 .duration(200)
                 .buildAndRegister();
+
         CHEMICAL_PLANT_RECIPES.recipeBuilder()
                 .fluidInputs(Biotin.getFluid(1000))
                 .fluidInputs(LinoleicAcid.getFluid(1000))
@@ -162,6 +190,7 @@ public class GrowthMedium {
                 .EUt(30720)
                 .duration(250)
                 .buildAndRegister();
+
         MIXER_RECIPES.recipeBuilder()
                 .fluidInputs(Ammonia.getFluid(1000))
                 .fluidInputs(DistilledWater.getFluid(1000))
@@ -169,20 +198,23 @@ public class GrowthMedium {
                 .EUt(1920)
                 .duration(100)
                 .buildAndRegister();
+
         BIO_REACTOR_RECIPES.recipeBuilder()
                 .fluidInputs(CleanAmmoniaSolution.getFluid(1000))
                 .inputs(BrevibacteriumFlavium.getItemStack())
                 .input(dust, Sugar)
-                .outputs(Glutamine.getItemStack(2))
+                .outputs(Glutamine.getItemStack(40))
                 .EUt(30720)
                 .duration(500)
                 .buildAndRegister();
+
         CENTRIFUGE_RECIPES.recipeBuilder()
                 .input(dust, Meat)
                 .fluidOutputs(Blood.getFluid(250))
                 .EUt(32)
                 .duration(50)
                 .buildAndRegister();
+
         CENTRIFUGE_RECIPES.recipeBuilder()
                 .fluidInputs(Blood.getFluid(1000))
                 .fluidOutputs(BloodCells.getFluid(450))
@@ -190,6 +222,7 @@ public class GrowthMedium {
                 .EUt(1920)
                 .duration(200)
                 .buildAndRegister();
+
         CENTRIFUGE_RECIPES.recipeBuilder()
                 .fluidInputs(BloodPlasma.getFluid(1000))
                 .fluidOutputs(Catalase.getFluid(10))
@@ -198,16 +231,18 @@ public class GrowthMedium {
                 .EUt(480)
                 .duration(50)
                 .buildAndRegister();
+
         CHEMICAL_PLANT_RECIPES.recipeBuilder()
                 .fluidInputs(B27Supplement.getFluid(1000))
                 .fluidInputs(AmmoniaNitrate.getFluid(1000))
-                .inputs(Glutamine.getItemStack())
+                .inputs(Glutamine.getItemStack(20))
                 .fluidInputs(BFGF.getFluid(1000))
                 .fluidInputs(EGF.getFluid(1000))
                 .fluidOutputs(RawGrowthMedium.getFluid(4000))
                 .EUt(30720)
                 .duration(500)
                 .buildAndRegister();
+
         MIXER_RECIPES.recipeBuilder()
                 .fluidInputs(BloodCells.getFluid(1000))
                 .notConsumable(ULTRASONIC_HOMOGENIZER.getStackForm())
