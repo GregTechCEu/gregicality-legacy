a03b348bdc0bf112bed6f8546fbb6033da34fff5
brachy84 <brachy84@gmail.com>
2021-03-28 20:50:45 +0200

    improved jei drilling rig info

--- src/main/java/gregicadditions/machines/multi/MetaTileEntityDrillingRig.java
+++ src/main/java/gregicadditions/machines/multi/MetaTileEntityDrillingRig.java
@@ -132,6 +132,7 @@ public class MetaTileEntityDrillingRig extends MultiblockWithDisplayBase {
         }
 
         if(isActive && canFillFluidExport() && !drainEnergy()) {
+            setActive(false);
             return;
         }
 
