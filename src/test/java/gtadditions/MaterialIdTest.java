5111da9890374e7c4d0c39c833f47e80ac92c24d
DStrand1 <dane@strandboge.com>
2021-04-16 15:42:21 -0500

    Create tests for MetaTileEntities

--- src/test/java/gtadditions/MaterialIdTest.java
+++ src/test/java/gtadditions/MaterialIdTest.java
@@ -2,11 +2,16 @@ package gtadditions;
 
 import gregicadditions.GAEnums;
 import gregicadditions.GAMaterials;
+import gregicadditions.fluid.GAMetaFluids;
+import gregicadditions.item.GAMetaBlocks;
+import gregicadditions.machines.GATileEntities;
 import gregtech.api.GTValues;
 import gregtech.api.unification.material.Materials;
 import gregtech.api.unification.material.type.Material;
 import gregtech.api.util.GTLog;
 import gregtech.common.MetaFluids;
+import gregtech.common.blocks.MetaBlocks;
+import gregtech.common.metatileentities.MetaTileEntities;
 import net.minecraft.init.Bootstrap;
 import org.apache.logging.log4j.LogManager;
 import org.junit.BeforeClass;
@@ -31,6 +36,13 @@ public class MaterialIdTest {
         gaMaterials.onMaterialsInit();
         Material.freezeRegistry();
         MetaFluids.init();
+        GAMetaFluids.init();
+
+        // Bootstrap the GTCE MetaTileEntity System
+        MetaBlocks.init();
+        GAMetaBlocks.init();
+        MetaTileEntities.init();
+        GATileEntities.init();
     }
 
     /**
@@ -48,6 +60,24 @@ public class MaterialIdTest {
         assertNotNull(
                 "OreDictUnifier failed to gather a Gregicality Material ItemStack",
                 GAMaterials.Tumbaga
+        );
+    }
+
+    /**
+     * Basic Nonnull test to try.
+     *
+     * The real test is in bootStrap, where if there are conflicting MTE ID's,
+     * it will throw an {@link IllegalArgumentException} and fail the test.
+     */
+    @Test
+    public void areMTEsGenerated() {
+        assertNotNull(
+                "GTCE MetaTileEntity is still null!",
+                MetaTileEntities.DIESEL_ENGINE
+        );
+        assertNotNull(
+                "Gregicality MetaTileEntity is still null!",
+                GATileEntities.ALLOY_BLAST_FURNACE
         );
     }
 }
