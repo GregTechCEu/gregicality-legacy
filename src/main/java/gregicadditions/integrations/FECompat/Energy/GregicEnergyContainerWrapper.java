5fc4ce01ebe9a42289bfe2cbad72eaee1a105ed1
DStrand1 <dane@strandboge.com>
2021-05-06 02:38:35 -0500

    Add RF buffer to machines to allow for partial amp consumption

--- src/main/java/gregicadditions/integrations/FECompat/Energy/GregicEnergyContainerWrapper.java
+++ src/main/java/gregicadditions/integrations/FECompat/Energy/GregicEnergyContainerWrapper.java
@@ -26,6 +26,7 @@ import javax.annotation.Nullable;
 
 import gregicadditions.GAConfig;
 import gregicadditions.GAValues;
+import gregicadditions.utils.GALog;
 import gregtech.api.capability.IEnergyContainer;
 import net.minecraft.util.EnumFacing;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
@@ -39,6 +40,8 @@ public class GregicEnergyContainerWrapper implements IEnergyContainer {
 
     private final ICapabilityProvider upvalue;
 	private final IEnergyStorage[] facesRF = new IEnergyStorage[7];
+
+	private int rfBuffer = 0;
 
 	public GregicEnergyContainerWrapper(ICapabilityProvider upvalue) {
 		this.upvalue = upvalue;
@@ -106,37 +109,114 @@ public class GregicEnergyContainerWrapper implements IEnergyContainer {
 		return container;
 	}
 
-	@Override
-	public long acceptEnergyFromNetwork(EnumFacing facing, long voltage, long amperage) {
-		int faceID = facing == null ? 6 : facing.getIndex();
-		IEnergyStorage container = facesRF[faceID];
+    @Override
+    public long acceptEnergyFromNetwork(EnumFacing facing, long voltage, long amperage) {
 
-		if (container == null) {
-			container = upvalue.getCapability(CapabilityEnergy.ENERGY, facing);
-			facesRF[faceID] = container;
-		}
+        int faceID = facing == null ? 6 : facing.getIndex();
+        IEnergyStorage container = facesRF[faceID];
 
-		if (container == null) {
-			return 0L;
-		}
+        if (container == null) {
+            container = upvalue.getCapability(CapabilityEnergy.ENERGY, facing);
+            facesRF[faceID] = container;
+        }
 
-		long maxPacket = voltage * Constants.RATIO_LONG;
-		long maximalValue = maxPacket * amperage;
+        if (container == null)
+            return 0L;
 
-		if (maximalValue > Integer.MAX_VALUE) {
-			maximalValue = Integer.MAX_VALUE;
-		}
+        int receive = 0;
 
-		int receive = container.receiveEnergy(safeCastLongToInt(maximalValue), true);
+        // Try to use the internal buffer before consuming a new packet
+        if (rfBuffer > 0) {
 
-		if (receive == 0)
-		    return 0L;
+            GALog.logger.info("Buffering");
 
-        if (receive < maxPacket)
-		    receive -= receive % maxPacket;
+            receive = container.receiveEnergy(rfBuffer, true);
 
-		return container.receiveEnergy(receive, false) / maxPacket;
-	}
+            if (receive == 0)
+                return 0;
+
+            // Internal Buffer could provide the max RF the consumer could consume
+            if (rfBuffer > receive) {
+                rfBuffer -= receive;
+                container.receiveEnergy(receive, false);
+                return 0;
+
+            // Buffer could not provide max value, save the remainder and continue processing
+            } else {
+                receive = rfBuffer;
+                rfBuffer = 0;
+            }
+        }
+
+        long maxPacket = voltage * Constants.RATIO_LONG;
+        long maximalValue = maxPacket * amperage;
+
+        // Try to consume our remainder buffer plus a fresh packet
+        if (receive != 0) {
+
+            GALog.logger.info("Remainder Buffer + New Amp");
+
+            int consumable = container.receiveEnergy(safeCastLongToInt(maximalValue + receive), true);
+
+            // Machine unable to consume any power
+            // Probably unnecessary in this case, but just to be safe
+            if (consumable == 0)
+                return 0;
+
+            // Only able to consume our buffered amount
+            if (consumable == receive) {
+                container.receiveEnergy(consumable, false);
+                return 0;
+            }
+
+            // Able to consume our full packet as well as our remainder buffer
+            if (consumable == maximalValue + receive) {
+                container.receiveEnergy(consumable, false);
+                return amperage;
+            }
+
+            int newPower = consumable - receive;
+
+            // Able to consume buffered amount plus an even amount of packets (no buffer needed)
+            if (newPower % maxPacket == 0) {
+                return container.receiveEnergy(consumable, false) / maxPacket;
+            }
+
+            // Able to consume buffered amount plus some amount of power with a packet remainder
+            int ampsToConsume = safeCastLongToInt((newPower / maxPacket) + 1);
+            rfBuffer = safeCastLongToInt((maxPacket * ampsToConsume) - consumable);
+            container.receiveEnergy(consumable, false);
+            return ampsToConsume;
+
+        // Else try to draw 1 full packet
+        } else {
+
+            GALog.logger.info("New Amp Only");
+
+            int consumable = container.receiveEnergy(safeCastLongToInt(maximalValue), true);
+
+            // Machine unable to consume any power
+            if (consumable == 0)
+                return 0;
+
+            // Able to accept the full amount of power
+            if (consumable == maximalValue) {
+                container.receiveEnergy(consumable, false);
+                return amperage;
+            }
+
+            // Able to consume an even amount of packets
+            if (consumable % maxPacket == 0) {
+                return container.receiveEnergy(consumable, false) / maxPacket;
+            }
+
+            // Able to consume power with some amount of power remainder in the packet
+            int ampsToConsume = safeCastLongToInt((consumable / maxPacket) + 1);
+            rfBuffer = safeCastLongToInt((maxPacket * ampsToConsume) - consumable);
+            container.receiveEnergy(consumable, false);
+            return ampsToConsume;
+        }
+    }
 
 	@Override
 	public long changeEnergy(long delta) {
