7271d9b0b009bc5f25acd0ecbb65cb3d562a6f0f
DStrand1 <dane@strandboge.com>
2021-06-10 22:49:24 -0500

    automatically apply solder fluids to circuit asm and assline

--- src/main/java/gregicadditions/recipes/impl/QubitConsumerRecipeBuilder.java
+++ src/main/java/gregicadditions/recipes/impl/QubitConsumerRecipeBuilder.java
@@ -1,6 +1,8 @@
 package gregicadditions.recipes.impl;
 
 import com.google.common.collect.ImmutableMap;
+import gregicadditions.GAConfig;
+import gregicadditions.GAUtility;
 import gregtech.api.recipes.Recipe;
 import gregtech.api.recipes.RecipeBuilder;
 import gregtech.api.recipes.RecipeMap;
@@ -8,7 +10,12 @@ import gregtech.api.util.EnumValidationResult;
 import gregtech.api.util.GTLog;
 import gregtech.api.util.ValidationResult;
 import mcp.MethodsReturnNonnullByDefault;
+import net.minecraftforge.fluids.FluidRegistry;
+import net.minecraftforge.fluids.FluidStack;
 import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @see Recipe
@@ -17,7 +24,21 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
 @MethodsReturnNonnullByDefault
 public class QubitConsumerRecipeBuilder extends RecipeBuilder<QubitConsumerRecipeBuilder> {
 
+    private static final List<FluidStack> SOLDER_FLUIDS = new ArrayList<>();
+
+    static {
+        for (String fluid : GAConfig.Misc.solderingFluidList) {
+            String[] fluidSplit = fluid.split(":");
+            int amount = GAUtility.setBetweenInclusive(Integer.parseInt(fluidSplit[1]), 1, 64000);
+
+            FluidStack fluidStack = FluidRegistry.getFluidStack(fluidSplit[0], amount);
+            if (fluidStack != null) SOLDER_FLUIDS.add(fluidStack);
+        }
+    }
+
     private int qubit;
+    private int solderMultiplier = 1;
+    private boolean noSolder = false;
 
     public QubitConsumerRecipeBuilder() {
     }
@@ -54,11 +75,34 @@ public class QubitConsumerRecipeBuilder extends RecipeBuilder<QubitConsumerRecip
         return this;
     }
 
+    public QubitConsumerRecipeBuilder solderMultiplier(int multiplier) {
+        this.solderMultiplier = multiplier;
+        return this;
+    }
+
+    public QubitConsumerRecipeBuilder noSolder() {
+        this.noSolder = true;
+        return this;
+    }
+
     public ValidationResult<Recipe> build() {
         return ValidationResult.newResult(finalizeAndValidate(),
                 new Recipe(inputs, outputs, chancedOutputs, fluidInputs, fluidOutputs,
                         ImmutableMap.of("qubitConsume", qubit),
                         duration, EUt, hidden));
+    }
+
+    @Override
+    public void buildAndRegister() {
+        if (!noSolder && fluidInputs.isEmpty()) {
+            for (FluidStack fluidStack : SOLDER_FLUIDS) {
+                recipeMap.addRecipe(this.copy()
+                        .fluidInputs(new FluidStack(fluidStack.getFluid(), Math.min(64000, fluidStack.amount * solderMultiplier)))
+                        .build());
+            }
+        } else {
+            recipeMap.addRecipe(build());
+        }
     }
 
     @Override
@@ -68,6 +112,4 @@ public class QubitConsumerRecipeBuilder extends RecipeBuilder<QubitConsumerRecip
                 .append("qubitConsume", qubit)
                 .toString();
     }
-
-
 }
