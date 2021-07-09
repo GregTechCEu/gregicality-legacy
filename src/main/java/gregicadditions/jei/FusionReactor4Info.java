e0efd4f65aadfe15e7353071444ec2eca4ce9a84
pablocnvs <64352555+pablocnvs@users.noreply.github.com>
2021-07-10 00:37:18 +0200

    Advanced Fusion Fixes
    Fixes initial EUt and adds tiers to the JEI preview, helpful for the freedom wrench

--- src/main/java/gregicadditions/jei/FusionReactor4Info.java
+++ src/main/java/gregicadditions/jei/FusionReactor4Info.java
@@ -14,7 +14,10 @@ import gregtech.integration.jei.multiblock.MultiblockShapeInfo;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.EnumFacing;
 
+import java.rmi.registry.Registry;
+import java.util.ArrayList;
 import java.util.List;
+;
 
 public class FusionReactor4Info extends MultiblockInfoPage {
     @Override
@@ -23,10 +26,14 @@ public class FusionReactor4Info extends MultiblockInfoPage {
         return GATileEntities.ADVANCED_FUSION_REACTOR;
     }
 
+
+
     @Override
     public List<MultiblockShapeInfo> getMatchingShapes() {
-        MultiblockShapeInfo shapeInfo = MultiblockShapeInfo.builder()
-                .aisle("###############", "###############","#####ccCcc#####","#####ccCcc#####","###############","###############")
+        ArrayList<MultiblockShapeInfo> shapeInfo = new ArrayList<>();
+        for (int i = 0; i < 5; i++){
+            GAMultiblockShapeInfo.Builder builder = GAMultiblockShapeInfo.builder();
+            builder.aisle("###############", "###############","#####ccCcc#####","#####ccCcc#####","###############","###############")
                 .aisle("###############", "#######C#######","###ccvEvEvcc###","###ccvEvEvcc###","#######C#######","###############")
                 .aisle("#######C#######", "##C##EEdEE##C##","##Cvv#####vvC##","##Cvv#####vvC##","##C##bbbbb##C##","#######C#######")
                 .aisle("###C###C###C###", "###EEdddddEE###","#cv#########vc#","#cv#########vc#","###bbbbbbbbb###","###C###C###C###")
@@ -43,16 +50,18 @@ public class FusionReactor4Info extends MultiblockInfoPage {
                 .aisle("###############", "###############","#####ccCcc#####","#####ccCcc#####","###############","###############")
                 .where('S', GATileEntities.ADVANCED_FUSION_REACTOR, EnumFacing.SOUTH)
                 .where('#', Blocks.AIR.getDefaultState())
-                .where('C', GAMetaBlocks.FUSION_CASING.getState(GAFusionCasing.CasingType.ADV_FUSION_COIL_1))
+                .where('C', GAMetaBlocks.FUSION_CASING.getState(GAFusionCasing.CasingType.values()[i + 4]))
                 .where('X', GAMetaBlocks.FUSION_CASING.getState(GAFusionCasing.CasingType.ADV_FUSION_CASING))
                 .where('f', MetaTileEntities.FLUID_IMPORT_HATCH[8], EnumFacing.SOUTH)
                 .where('F', MetaTileEntities.FLUID_EXPORT_HATCH[8], EnumFacing.SOUTH)
                 .where('E', GATileEntities.ENERGY_INPUT[0], EnumFacing.NORTH)
-                .where('c', GAMetaBlocks.CRYOSTAT_CASING.getState(GACryostatCasing.CasingType.CRYOSTAT_1))
-                .where('v', GAMetaBlocks.VACUUM_CASING.getState(GAVacuumCasing.CasingType.VACUUM_1))
+                .where('c', GAMetaBlocks.CRYOSTAT_CASING.getState(GACryostatCasing.CasingType.values()[i]))
+                .where('v', GAMetaBlocks.VACUUM_CASING.getState(GAVacuumCasing.CasingType.values()[i]))
                 .where('b', GAMetaBlocks.FUSION_CASING.getState(GAFusionCasing.CasingType.FUSION_BLANKET))
-                .where('d', GAMetaBlocks.DIVERTOR_CASING.getState(GADivertorCasing.CasingType.DIVERTOR_1))
+                .where('d', GAMetaBlocks.DIVERTOR_CASING.getState(GADivertorCasing.CasingType.values()[i]))
                 .build();
+            shapeInfo.add(builder.build());
+        }
         return Lists.newArrayList(shapeInfo);
     }
 
