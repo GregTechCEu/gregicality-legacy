52ede826f6504de1c270e16f31ee73b57a0a131d
Irgendwer01 <67506320+Irgendwer01@users.noreply.github.com>
2021-08-09 11:16:08 +0200

    Add Native EU to RF Conversion (#479)
    * Add Native EU to RF/FE conversion
    
    NOT Vice-versa
    
    * Add Config option for Ratio and disable EU to RF Energy Converter by Default
    
    * Readd Licensing notes and replace GTValues through GAValues
    
    * Add some safer long to int casting
    
    * Fix packet too large not being used
    
    This means a packet too large than the max value of RF to receive will have its partial voltage burned. This is preferable to just not powering the machine at all, but we could make an RF buffer in the cable to do better
    
    * Refactor, remove redundant code in EnergyContainerWrapper
    
    * Move capability inject to GregicAdditionsCapabilities
    
    * Lock capability to wrap only GTEU producers
    
    Need to fix wires/cables lacking capability
    
    * Add RF buffer to machines to allow for partial amp consumption
    
    * remove unneeded changes
    
    * Start TOP fix
    
    * Remove License Notes as requested
    
    * commit stash
    
    * cleanup, drop item capabilities for a later PR
    
    * Add config to disable feature fully
    
    * small cleanup
    
    * comments, some more cleanup
    
    * fix potential overflow issue
    
    * Add proper synchronization locking
    
    * address a few requested changes
    
    * Address more changes, rework config
    
    * Clarify ratio config slightly
    
    * Fix missed error
    
    Co-authored-by: DStrand1 <dane@strandboge.com>
    Co-authored-by: hjae78 <54247183+hjae78@users.noreply.github.com>

--- src/main/java/gregicadditions/capabilities/GregicAdditionsCapabilities.java
+++ src/main/java/gregicadditions/capabilities/GregicAdditionsCapabilities.java
@@ -1,13 +1,21 @@
 package gregicadditions.capabilities;
 
+import gregicadditions.Gregicality;
+import gregicadditions.integrations.FECompat.EnergyProvider;
 import gregicadditions.machines.multi.multiblockpart.MetaTileEntityMaintenanceHatch;
 import gregicadditions.machines.multi.multiblockpart.MetaTileEntityMufflerHatch;
 import gregtech.api.metatileentity.multiblock.MultiblockAbility;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.CapabilityInject;
+import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.fluids.IFluidTank;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.items.IItemHandlerModifiable;
 
+@Mod.EventBusSubscriber(modid = Gregicality.MODID)
 public class GregicAdditionsCapabilities {
 
 
@@ -24,6 +32,12 @@ public class GregicAdditionsCapabilities {
     public static final MultiblockAbility<IItemHandlerModifiable> STEAM_IMPORT_ITEMS = new MultiblockAbility<>();
     public static final MultiblockAbility<IItemHandlerModifiable> STEAM_EXPORT_ITEMS = new MultiblockAbility<>();
 
+    private static final ResourceLocation CAPABILITY_EU_TO_FE = new ResourceLocation(Gregicality.MODID, "fecapability");
+
+    @SubscribeEvent
+    public static void attachTileCapability(AttachCapabilitiesEvent<TileEntity> event) {
+        event.addCapability(CAPABILITY_EU_TO_FE, new EnergyProvider(event.getObject()));
+    }
     public static final MultiblockAbility<MetaTileEntityMaintenanceHatch> MAINTENANCE_HATCH = new MultiblockAbility<>();
     public static final MultiblockAbility<MetaTileEntityMufflerHatch> MUFFLER_HATCH = new MultiblockAbility<>();
 
--- src/main/java/gregicadditions/machines/energyconverter/utils/EnergyConverterType.java
+++ src/main/java/gregicadditions/machines/energyconverter/utils/EnergyConverterType.java
@@ -5,56 +5,86 @@ import gregicadditions.recipes.helper.GACraftingComponents;
 import gregtech.api.items.OreDictNames;
 
 public enum EnergyConverterType implements EnergyConverterCraftingHelper.RecipeFunction {
-	CONVERT_GTEU(ConverterType.GTEU_TO_FORGE, true) {
-		@Override
-		public Object[] createRecipe(final int tier, final int invSize) {
-			return new Object[]{"WTW", "RMR", "WSW", 'M', GACraftingComponents.HULL.getIngredient(tier), 'W', EnergyConverterCraftingHelper.HELPER.cable(tier, invSize), 'T', OreDictNames.chestWood, 'R', EnergyConverterCraftingHelper.HELPER.redCable(invSize), 'S', GACraftingComponents.CIRCUIT.getIngredient(tier)};
-		}
-	},
-	CONVERT_FORGE(ConverterType.GTEU_TO_FORGE, false) {
-		@Override
-		public Object[] createRecipe(final int tier, final int invSize) {
-			return new Object[]{"WSW", "RMR", "WTW", 'M', GACraftingComponents.HULL.getIngredient(tier), 'W', EnergyConverterCraftingHelper.HELPER.cable(tier, invSize), 'T', OreDictNames.chestWood, 'R', EnergyConverterCraftingHelper.HELPER.redCable(invSize), 'S', GACraftingComponents.CIRCUIT.getIngredient(tier)};
-		}
-	};
 
-	private final ConverterType type;
-	private final boolean isGTEU;
+    CONVERT_GTEU(ConverterType.GTEU_TO_FORGE, true) {
+        @Override
+        public Object[] createRecipe(final int tier, final int invSize) {
+            return new Object[]{
+                    "WTW", "RMR", "WSW",
+                    'M', GACraftingComponents.HULL.getIngredient(tier),
+                    'W', EnergyConverterCraftingHelper.HELPER.cable(tier, invSize),
+                    'T', OreDictNames.chestWood,
+                    'R', EnergyConverterCraftingHelper.HELPER.redCable(invSize),
+                    'S', GACraftingComponents.CIRCUIT.getIngredient(tier)
+            };
+        }
+    },
 
-	EnergyConverterType(final ConverterType type, final boolean isGTEU) {
-		this.type = type;
-		this.isGTEU = isGTEU;
-	}
+    CONVERT_FORGE(ConverterType.GTEU_TO_FORGE, false) {
+        @Override
+        public Object[] createRecipe(final int tier, final int invSize) {
+            return new Object[]{
+                    "WSW", "RMR", "WTW",
+                    'M', GACraftingComponents.HULL.getIngredient(tier),
+                    'W', EnergyConverterCraftingHelper.HELPER.cable(tier, invSize),
+                    'T', OreDictNames.chestWood,
+                    'R', EnergyConverterCraftingHelper.HELPER.redCable(invSize),
+                    'S', GACraftingComponents.CIRCUIT.getIngredient(tier)
+            };
+        }
+    };
 
-	public ConverterType getConverterType() {
-		return this.type;
-	}
+    private final ConverterType type;
+    private final boolean isGTEU;
 
-	public boolean isGTEU() {
-		return this.isGTEU;
-	}
+    EnergyConverterType(final ConverterType type, final boolean isGTEU) {
+        this.type = type;
+        this.isGTEU = isGTEU;
+    }
 
-	public Energy getInput() {
-		return this.type.getInput(this.isGTEU);
-	}
+    public ConverterType getConverterType() {
+        return this.type;
+    }
 
-	public Energy getOutput() {
-		return this.type.getOutput(this.isGTEU);
-	}
+    public boolean isGTEU() {
+        return this.isGTEU;
+    }
 
-	public Ratio ratio() {
-		return type.getOutput(isGTEU) == Energy.FE ?
-				Ratio.ratioOf(1, GAConfig.energyConverter.RatioEUtoRF) : //EU to RF
-				Ratio.ratioOf(GAConfig.energyConverter.RatioRFtoEU, 1); //RF to EU
-	}
+    public Energy getInput() {
+        return this.type.getInput(this.isGTEU);
+    }
 
-	public boolean isDisabled() {
-		return this.isGTEU() ? GAConfig.energyConverter.disableEUtoRF : GAConfig.energyConverter.disableRFtoEU;
-	}
+    public Energy getOutput() {
+        return this.type.getOutput(this.isGTEU);
+    }
 
+    public Ratio ratio() {
 
-	@Override
-	public String toString() {
-		return this.name().toLowerCase();
-	}
+        // 1 EU == many RF
+        if (GAConfig.EnergyConversion.RATIO > 1) {
+            return type.getOutput(isGTEU) == Energy.FE ?
+                    Ratio.ratioOf(1, (long) GAConfig.EnergyConversion.RATIO) : // EU to FE
+                    Ratio.ratioOf((long) GAConfig.EnergyConversion.RATIO, 1); // FE to EU
+
+        // 1 RF == many EU
+        } else if (GAConfig.EnergyConversion.RATIO < 1) {
+            int conversionRate = (int) (1 / GAConfig.EnergyConversion.RATIO);
+            return type.getOutput(isGTEU) == Energy.FE ?
+                    Ratio.ratioOf(conversionRate, 1) : // EU to FE
+                    Ratio.ratioOf(1, conversionRate);   // FE to EU
+
+        // EU to FE (and vice versa) are 1:1
+        } else return Ratio.ratioOf(1, 1);
+    }
+
+    public boolean isDisabled() {
+        return this.isGTEU() ?
+                GAConfig.EnergyConversion.disableEUtoFEConverter :
+                GAConfig.EnergyConversion.disableFEtoEUConverter;
+    }
+
+    @Override
+    public String toString() {
+        return this.name().toLowerCase();
+    }
 }
